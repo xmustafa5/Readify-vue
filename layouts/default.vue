fcda45ef132da0e6ed023a520ec21b3fff2a2f53
mustafa <musmoh73@gmail.com>
2023-10-30 15:54:09 +0300

    back image

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,21 @@
 <template>
+    <div :style="{}" class="navb">
+       v <!-- <img src="../assets//afterPS//1.png" alt="ss"> -->
+    </div>
     <div>
-        
+        <slot/>
     </div>
 </template>
+
+<style  scoped>
+
+.navb{
+    width: 100%;
+    height: 100vh;
+    background: url(../assets/afterPS/10.png) no-repeat;
+    background-size: cover ; /*غلمود تتعدل*/
+    background-position: center ; /*غلمود تتعدل*/
+
+}
+/* responsive */
+</style>>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,13 @@
 <template>
     <h1>home</h1>
 </template>
+
+<style>
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    scroll-behavior: smooth;
+    background: #F5DEAF;
+}
+</style>
