a255f4db5bf0f3d02bcb741d6b1ccf7e1ad5290c
mustafa <musmoh73@gmail.com>
2023-11-03 15:30:35 +0300

    make filter

---
+++ pages/Crime.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>horror</h1>
+        <div>
+            <div v-for="b in horror" :key="b.id">
+                {{ b.title }}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup >
+const {data: books} = await useFetch('http://localhost:3002/books')
+const horror = computed(() => {
+  return  books.value.filter(t => t.type === 'horror')
+
+})
+
+</script>
+
+<style scoped>
+
+</style>
