c055af4c92ff98397bf148ceec91ae76bddebdf8
mustafa <musmoh73@gmail.com>
2023-11-04 13:53:07 +0300

    make store

---
+++ stores/store.ts
@@ -0,0 +1,28 @@
+import {defineStore} from "pinia"
+// Define an interface for your store state
+interface Book {
+    id: number;
+    title: string;
+    description: string;
+    type: string;
+    isFav: boolean;
+  }
+  
+  export const useFetch = defineStore({
+    id: 'fetch',
+    state: () => ({
+      mustafa: "mustafa" as string,
+      loading: false as boolean,
+      books: [] as Book[]
+    }),
+    actions: {
+      async getTasks() {
+        this.loading = true;
+        const res = await fetch('http://localhost:3001/books');
+        const data = await res.json();
+        this.books = data;
+        this.loading = false;
+      },
+    },
+  });
+  
